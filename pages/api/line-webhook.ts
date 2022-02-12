f1b8792a5aaf40c8156c79cd6d0f13f7ef865e3a
つよし <70616489+pon0204@users.noreply.github.com>
2022-02-12 22:56:22 +0900

    友だち追加時にユーザーデータをDBに保存 (#3)
    * chore: line-bot-sdk インストール
    
    * feat: line-webhookサーバーモック実装
    
    * feat: webhook署名検証実装
    
    * feat: LineUserテーブル追加
    
    * feat: 友だち追加時にDBにデータ保存
    
    * prismaの型追加
    
    * prisma.$connect()を追加
    
    * css削除

---
+++ pages/api/line-webhook.ts
@@ -0,0 +1,50 @@
+import { Client as LineClient, middleware } from '@line/bot-sdk'
+import {
+  validateSignature,
+  WebhookEvent,
+  WebhookRequestBody,
+} from '@line/bot-sdk'
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const lineConfig = {
+  channelId: process.env.LINE_CHANNEL_ID || '',
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS || '',
+  channelSecret: process.env.LINE_CHANNEL_SECRET || '',
+}
+
+const prisma = new PrismaClient()
+prisma.$connect()
+const lineClient = new LineClient(lineConfig)
+
+export default async function lineWebhook(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const body: WebhookRequestBody = req.body
+  const events: WebhookEvent[] = req.body.events
+  const sig = req.headers['x-line-signature'] as string
+  try {
+    validateSignature(JSON.stringify(req.body), lineConfig.channelSecret, sig)
+    switch (events[0].type) {
+      case 'follow':
+        addUser(events[0])
+        break
+    }
+    res.json({})
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function addUser(event: WebhookEvent) {
+  const userId = event.source.userId as string
+  const profile = await lineClient.getProfile(userId)
+
+  await prisma.lineUser.create({
+    data: {
+      userId,
+      name: profile.displayName,
+    },
+  })
+}
